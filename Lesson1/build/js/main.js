c3298dba03b75fcad2c127c533564c3b29009769
Naveen Kumar <naveenchaurasiya1501@gmail.com>
2023-07-05 11:12:22 +0530

    Typescript config and basics

---
+++ Lesson1/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let myName = "Naveen";
+myName = "Name";
+console.log(myName);
+let one = 1;
+let two = "2";
+let three = true;
+// console.log(two / one);
---
+++ Lesson1/src/main.ts
@@ -0,0 +1,9 @@
+let myName = "Naveen";
+
+myName = "Name";
+console.log(myName);
+let one: number = 1;
+let two: string = "2";
+let three: boolean = true;
+
+console.log(two / one);
